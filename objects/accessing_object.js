d7716f3b61b1c4d9da16c08d9be4c6f5b7c04672
Kumfa Jina <besibangk@gmail.com>
2023-09-21 03:13:07 -0700

    accessing objects in js

---
+++ objects/accessing_object.js
@@ -0,0 +1,12 @@
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 42;  // Change this line
+const player = testObj;   
+
+console.log(testObj[16]);
